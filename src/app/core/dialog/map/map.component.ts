56c2998b3fce6e2ccf167476fe38c35913f3ff62
pst-davide <davide.sorrentino@pasubiotecnologia.it>
2024-09-11 17:28:09 +0200

    Lavoro 11.09.24

---
+++ src/app/core/dialog/map/map.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements OnInit {
+
+  ngOnInit(): void {
+    new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+}
