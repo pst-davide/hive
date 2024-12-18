8f3edd2ba9a7fba82e4789a2f9c7028f0915ea0b
pst-davide <davide.sorrentino@pasubiotecnologia.it>
2024-12-18 13:02:07 +0100

    press keywords controller

--- server/src/controllers/press-keyword.controller.ts
+++ server/src/controllers/press-keyword.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { PressKeyword } from '../models/press-keyword.model';
 import { PressCategory } from '../models/press-category.model';
+import logger from '../utils/logger';
 
 export class PressKeywordController {
+
   static async findAll(req: Request, res: Response): Promise<void> {
+    logger.info('Ottieni tutte le keywords');
     try {
       const { categoryId } = req.query;
 
@@ -23,14 +26,17 @@ export class PressKeywordController {
 
       res.status(200).json(keywords);
     } catch (error: any) {
+      logger.error('Errore durante il recupero delle keywords:', { error: error.message });
       res.status(500).json({ error: 'Errore durante il recupero delle keywords', details: error.message });
     }
   }
 
   static async findById(req: Request, res: Response): Promise<void> {
     const id: number = parseInt(req.params['id'], 10);
+    logger.info(`Ottieni la keyword con ID: ${id}`);
 
     if (isNaN(id)) {
+      logger.warn(`Keyword con ID: ${id} non valida`);
       res.status(400).json({ error: 'ID non valido' });
       return;
     }
@@ -48,35 +54,47 @@ export class PressKeywordController {
       if (keyword) {
         res.status(200).json(keyword);
       } else {
+        logger.warn(`Keyword con ID: ${id} non trovata`);
         res.status(404).json({ error: 'Keyword non trovata' });
       }
     } catch (error: any) {
+      logger.error('Errore durante il recupero della keyword:', { error: error.message });
       res.status(500).json({ error: 'Errore durante il recupero della keyword', details: error.message });
     }
   }
 
   static async create(req: Request, res: Response): Promise<void> {
+    logger.info('Creazione di una nuova keyword con i dati:', req.body);
+
     try {
       const keyword: PressKeyword = await PressKeyword.create(req.body);
+      logger.info('Keyword creata con successo:', keyword.id);
       res.status(201).json(keyword);
     } catch (error: any) {
+      logger.error('Errore durante la creazione della keyword:', { error: error.message });
       res.status(500).json({ error: 'Errore durante la creazione della keyword', details: error.message });
     }
   }
 
   static async createBatch(req: Request, res: Response): Promise<void> {
+    logger.info('Creazione batch di keywords con i dati:', req.body);
+
     try {
       const keywords: PressKeyword[] = await PressKeyword.bulkCreate(req.body, { validate: true });
+      logger.info('Keywords create con successo:', keywords);
       res.status(201).json(keywords);
     } catch (error: any) {
+      logger.error('Errore durante la creazione delle keywords in batch:', { error: error.message });
       res.status(500).json({ error: 'Errore durante la creazione delle keywords in batch', details: error.message });
     }
   }
 
   static async update(req: Request, res: Response): Promise<void> {
     const id: number = parseInt(req.params['id'], 10);
+    logger.info(`Aggiornamento della keyword con ID: ${id}, dati ricevuti:`, req.body);
 
     if (isNaN(id)) {
+      logger.warn(`Keyword con ID: ${id} non valida`);
       res.status(400).json({ error: 'ID non valido' });
       return;
     }
@@ -86,11 +104,14 @@ export class PressKeywordController {
 
       if (keyword) {
         await keyword.update(req.body);
+        logger.info('Keyword aggiornata con successo:', keyword.id);
         res.status(200).json(keyword);
       } else {
+        logger.warn(`Keyword con ID: ${id} non trovata`);
         res.status(404).json({ error: 'Keyword non trovata' });
       }
     } catch (error: any) {
+      logger.error('Errore durante l\'aggiornamento della keyword:', { error: error.message });
       res.status(500).json({ error: 'Errore durante l\'aggiornamento della keyword', details: error.message });
     }
   }
